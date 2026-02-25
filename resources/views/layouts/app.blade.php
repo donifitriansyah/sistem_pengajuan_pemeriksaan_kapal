44498458c76ecff34f0a3d3c9d9d6d171d1ee27f
Doni Fitriansyah <donifitriansyah11tkj@gmail.com>
2026-02-25 11:10:45 +0700

    favicon fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <base target="_top">
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon" />
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
 
     <title>@yield('title')</title>
     @include('includes.backend.style')
